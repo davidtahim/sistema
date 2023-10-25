3584beb2f303f0147dd0893060a09d6840617840
David Tahim <davidtahim@gmail.com>
2023-10-25 16:54:14 -0300

    conserto

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if ($_SERVER ["REQUEST_METHOD"] == "POST") {
 	$senha = $_POST['senha']; 
 }
 
-$sql = "SELECT * FROM `usuarios` WHERE `email`;";
+$sql = "SELECT * FROM `usuarios` WHERE email = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("s", $email);
 $stmt->execute();
